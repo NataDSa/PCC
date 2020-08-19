401a6d42305660a41fa138076aeca0a47db819c7
Vitaly <vdubov@simtechdev.com>
2020-08-19 16:37:23 +0400

    fix

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/{slug}', function () {
     return view('welcome');
-});
-
-// Auth::routes();
-
-// Route::get('/home', 'HomeController@index')->name('home');
-
+})->where('slug', '.*');
