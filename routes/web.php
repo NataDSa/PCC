4b17fd4da7942d892d8fa7e3d343c76e2383cb16
NataDSa <salnikova.nata98@yandex.ru>
2020-10-06 20:50:41 +0300

    nova

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/{slug}', function () {
+Route::get('/{any}', function () {
     return view('welcome');
-})->where('slug', '.*');
+})->where('any', '^(?!careadmin|nova-api).*$');
